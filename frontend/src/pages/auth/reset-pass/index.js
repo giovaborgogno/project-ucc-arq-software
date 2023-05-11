380d5779f3fc573adcddd0b68ef92de554780760
giovaborgogno <giovaborgogno@gmail.com>
2023-05-10 23:39:31 -0300

    Auth front

---
+++ frontend/src/pages/auth/reset-pass/index.js
@@ -0,0 +1,11 @@
+
+import MainLayout from '@/layouts/MainLayout'
+export default function ResetPass() {
+  return (
+    <>
+      <MainLayout title={"Reset Password"}>
+        ResetPass
+      </MainLayout>
+    </>
+  )
+}
