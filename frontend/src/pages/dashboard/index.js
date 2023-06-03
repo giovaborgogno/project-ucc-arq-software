b4f2905060848fba5cc44005c1cd9f65657f8b00
felipecanias <felacanas2003@gmail.com>
2023-06-03 11:51:20 -0300

    Banner, Bookingcard, Userinfo, GetUsers

--- frontend/src/pages/dashboard/index.js
+++ frontend/src/pages/dashboard/index.js
@@ -2,17 +2,26 @@ import DashboardUserProfile from "@/components/dashboard/DashboardUser"
 import DashboardUser from "@/layouts/DashboardUser"
 import { UserContext } from "@/layouts/LayoutContext"
 import MainLayout from "@/layouts/MainLayout"
+import { getMe } from "@/lib/api/user"
 import { useRouter } from "next/router"
 import { useContext, useEffect } from "react"
 
 const Dashboard = () => {
   const [user, setUser] = useContext(UserContext);  
+  const [userdata, setUserdata] = useContext(UserContext); 
   const router = useRouter()
 
   useEffect(()=>{
 
     if(user === null)
     router.push("/auth/login")
+
+    const get_me = async () => {
+      const data = await getMe()
+      setUserdata(data)
+    }
+    get_me()
+
   },[])
 
   return (
@@ -21,7 +30,7 @@ const Dashboard = () => {
 
         {/* Replace with your content */}
           {/* <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" /> */}
-        <DashboardUserProfile />
+        <DashboardUserProfile userdata = {userdata} />
         {/* /End replace */}
         
       </DashboardUser>
