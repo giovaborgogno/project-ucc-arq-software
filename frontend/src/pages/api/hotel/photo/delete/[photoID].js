aeb0ed28b02cf23350e1b2ed11e3fe9b5241a065
giovaborgogno <giovaborgogno@gmail.com>
2023-06-05 11:26:04 -0300

    update amenities and photos on HotelDetail page

---
+++ frontend/src/pages/api/hotel/photo/delete/[photoID].js
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default async function handler(req, res) {
+    try {
+      const { photoID } = req.query;
+
+      const response = await fetch(`${process.env.NEXT_PUBLIC_URL_API}/api/hotel/photo/${photoID}`, {
+        method: 'DELETE',
+        headers: {
+          ...req.headers.JSON,
+          'Cookie': req.headers.cookie || ''
+        },
+        credentials: 'include',
+      });
+  
+      const data = await response.json();
+      res.status(response.status).json(data);
+    } catch (error) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  }
+  
